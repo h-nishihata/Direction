5d806df070a6dca9eda34c25bb5dc319720b8480
h-nishihata <nishihata0301@gmail.com>
2019-09-14 12:50:15 +0900

    add UI

---
+++ Assets/ValueSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ValueSetter : MonoBehaviour
+{
+    public Material[] mats;
+    public Slider[] sliders;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < mats.Length; i++)
+        {
+            mats[i].SetFloat("_Frequency", sliders[0 + i * 2].value);
+            mats[i].SetFloat("_Fill", sliders[1 + i * 2].value);
+            //mats[i].SetFloat("_LineColor", shininess);
+            //mats[i].SetFloat("_BGColor", shininess);
+        }
+    }
+}
