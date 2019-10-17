a63270a72d84a6ef939bb24f36d8b566655f23ca
h-nishihata <nishihata0301@gmail.com>
2019-10-17 23:20:46 +0900

    fix

--- Assets/Direction/Scripts/UISwitcher.cs
+++ Assets/Direction/Scripts/UISwitcher.cs
@@ -4,39 +4,15 @@ using UnityEngine;
 public class UISwitcher : MonoBehaviour
 {
     public Transform UIPanel;
-    public BoxCollider collider;
-    private Vector3 colCenterHalf = new Vector3(-2.5f, 0f, 0f);
-    private Vector3 colSizeHalf = new Vector3(5f, 3f, 10f);
-    private Vector3 colSizeFull = new Vector3(10f, 3f, 10f);
-    private bool isEnabled = true;
+    private bool UIActive = true;
     private bool shutterPressed;
     private float waitForScreenshot;
 
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                //Debug.Log("hit");
-                switchUIPanel(isEnabled = !isEnabled);
-                if(isEnabled)
-                {
-                    collider.center = colCenterHalf;
-                    collider.size = colSizeHalf;
-                }
-                else if(!isEnabled)
-                {
-                    collider.center = Vector3.zero;
-                    collider.size = colSizeFull;
-                }
-            }
-        }
+        if (!UIActive && Input.GetMouseButtonDown(0))
+            switchUIPanel(true);
 
         if (shutterPressed)
         {
@@ -54,6 +30,7 @@ public class UISwitcher : MonoBehaviour
     public void switchUIPanel(bool isActive)
     {
         UIPanel.gameObject.SetActive(isActive);
+        UIActive = isActive;
     }
 
     public void Screenshot()
